51f6c5c7202c26a5e5488f2fdc62aff93c7b9d30
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-14 16:02:40 +0200

    Install cancancan and configure authorizations

---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, User
+
+    return unless user.present?
+
+    can :manage, Group, user_id: user.id
+    can :manage, Payment, user_id: user.id
+  end
+end

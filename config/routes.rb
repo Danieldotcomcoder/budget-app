c55221066db97a9fbcfa2f43d9971a919007e44f
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-14 17:32:28 +0200

    Fix Sign out issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "users#index"
+  root to: "splash#index"
 
   resources :groups, only: [:index, :show, :new, :create] do
     resources :payments, only: [:new, :create]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end

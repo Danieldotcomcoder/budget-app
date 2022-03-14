9c9c9feeb18e2c3fcac1d4eb1c64d125ba868557
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-14 16:19:59 +0200

    Add relations and validations

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,9 @@
 class Payment < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :group
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
 end
